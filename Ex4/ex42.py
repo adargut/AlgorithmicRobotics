2d11dba608956ec32b8bb7cf631de929a661ad61
Tomer Davidor <tomer.davidor@gmail.com>
2020-01-18 21:19:51 +0200

    Added RRT full implementation with collision and stuff

--- Ex4/ex42.py
+++ Ex4/ex42.py
@@ -1,18 +1,31 @@
 from arr2_epec_seg_ex import *
 import networkx as nx
 import math
+import random
 import numpy as np
+import linear_path_intersection_test
+import tqdm
+
+N = 100
+
 
 class Planner(object):
-    def __init__(self, robots, obstacles):
+    def __init__(self, robots, obstacles, destinations):
         self.robots = list(map(Polygon_2, robots))
-        self.obstacles = list(map(Polygon_2, obstacles))
+        self.obstacles = Polygon_set_2()
+        self.obstacles.insert_polygons(list(map(Polygon_2, obstacles)))
+        
+        self.destinations = destinations
         self._kd_tree = Kd_tree()
+        self._neighbors_buffer = []
         self._graph = nx.DiGraph()
         self._bounds = self._compute_bounds()
 
+        self._aabb = {}
+        
+
     def _compute_bounds(self):
-        minX, maxX, minY, maxY = math.inf, -math.inf, math.inf, -math.inf
+        minX, maxX, minY, maxY = FT(math.inf), FT(-math.inf), FT(math.inf), FT(-math.inf)
 
         for obstacle in self.obstacles:
             for vertex in obstacle.vertices():
@@ -23,46 +36,91 @@ class Planner(object):
                 maxY = max(maxY, y)
         return [minX, maxX, minY, maxY]
 
-    def _find_k_nearest_neighbors(self, query_point, k):
-        # Point_d demonstration
-        p_d = Point_d(4, [FT(1), FT(2), FT(3), FT(4)])
-        print(p_d.dimension())
-        print(p_d.cartesian(0))
-        print(p_d[2])
 
-        tree = Kd_tree([p_d])
-        lst = []
-        tree.points(lst)
-        for i in range(10):
-            tree.insert(Point_d(4, [FT(i), FT(i), FT(i), FT(i)]))
-
-        query = Point_d(4, [FT(1), FT(2), FT(3), FT(4)])
-        k = 5
+    def _find_k_nearest_neighbors(self, query_point, k=5):
         eps = FT(Gmpq(0.0))  # 0.0 for exact NN, otherwise approximate NN
         search_nearest = True  # set this value to False in order to search farthest
         sort_neighbors = False  # set this value to True in order to obtain the neighbors sorted by distance
 
-        search = K_neighbor_search(tree, query, k, eps, search_nearest, \
+        search = K_neighbor_search(self._kd_tree, query, k, eps, search_nearest, \
                                    Euclidean_distance(), sort_neighbors)
 
         lst = []
         search.k_neighbors(lst)
+        return lst
 
     def _is_collision_free(self, source, target):
-        raise NotImplemented
+        return True # FIXME: Actually check if the point is in a free space
+        s1 = Point_2(source[0], source[1])
+        s2 = Point_2(source[1], source[2])
 
-    def generate_path(self, destinations):
-        return [[Point_2(1, 1), Point_2(2, 2)], [Point_2(3, 5), Point_2(5, 1)]]
+        t1 = Point_2(target[0], target[1])
+        t2 = Point_2(target[1], target[2])
 
-    def RRT(self, source, target, n, eta):
+        # move robot 1
+        r1_start = minkowski_sum_polygon_point(self.robots[0], s1)
+        r1_dest = minkowski_sum_polygon_point(self.robots[0], t1)
+        r1_movement = convex_hull_2([r1_start, r1_dest])
+
+        # move robot 2
+        r2_start = minkowski_sum_polygon_point(self.robots[1], s2)
+        r2_dest = minkowski_sum_polygon_point(self.robots[1], t2)
+        r2_movement = convex_hull_2([r2_start, r2_dest])
+
+        if self.obstacles.do_intersect(r1_movement) or self.obstacles.do_intersect(r2_movement):
+            return False
+
+        return not linear_path_intersection_test.do_intersect(self.robots[0], self.robots[1], [s1, s2], [t1, t2])
+        
+    def _is_valid_point(self, point):
+        return True  # FIXME: Actually check if the point is in a free space
+        p1 = Point_2(point[0], point[1])
+        p2 = Point_2(point[1], point[2])
+
+        r1 = minkowski_sum_polygon_point(self.robots[0], p1)
+        r2 = minkowski_sum_polygon_point(self.robots[1], p2)
+
+        
+        if self.obstacles.do_intersect(r1) or self.obstacles.do_intersect(r2):
+            return False
+        
+        ps = Polygon_set_2(p1)
+        return not ps.do_intersect(p2)
+
+    # def calculate_free_space():
+    #     self._aabb_dict = {Bbox_2(obstacle.vertices()): obstacle for obstacle in self.obstacles()}
+
+    def _add_node_to_tree(self, node):
+        self._graph.add_node(node)
+        self._kd_tree.insert(node)  # FIXME: inefficient!
+
+    def generate_path(self, n):
+        midpoint_1 = sum(self.robots[0].vertices()) / FT(self.robots[0].size())
+        midpoint_2 = sum(self.robots[1].vertices()) / FT(self.robots[1].size())
+        source = Point_d(4, [midpoint_1[0], midpoint_1[1], midpoint_2[0], midpoint_2[1]])
+
+        self._kd_tree = Kd_tree([source])
+        self._graph.add_node(source)
+
+        target = Point_d(4, self.destinations[0].extend(self.destinations[1]))
+
+        self.RRT(source, self.destinations, target, n)
+
+        path = nx.shortest_path(self._graph, source, target)
+        if len(path) > 0:
+            path = [[Point_2(p[0], p[1]), Point_2(p[2], p[3])] for p in path]
+        return path
+        #return [[Point_2(1, 1), Point_2(2, 2)], [Point_2(3, 5), Point_2(5, 1)]]
+
+    def RRT(self, source, target, n, eta=1):
         self._graph.clear()
         self._graph.add_node(source)
-        for j in range(n):
+        for j in tqdm.tqdm(range(n)):
             rand = self._get_free_sample()
-            near = self._find_k_nearest_neighbors(rand, 1)
+            near, _ = self._find_k_nearest_neighbors(rand, k=1)
             new = self._steer(near, rand, eta)
             if self._is_collision_free(near, new):
-                self._graph.add_node(new)
+                self._add_node_to_tree(new)
                 self._graph.add_edge(near, new)
 
     def RRT_star(self, source, target, n, eta):
@@ -101,7 +159,37 @@ class Planner(object):
                             self._graph.add_edge(new_point, near)
 
     def _get_free_sample(self):
-        raise NotImplemented
+        sample = None
+        valid = False
+        GOAL_BIAS = 5
+        if np.random.randint(0, 100) <= GOAL_BIAS:
+            return Point_d(4, [self.destinations[0].x(), self.destinations[0].y(), self.destinations[1].x(), self.destinations[1].y()])
+        while not valid:
+            robot_1 = self.generate_random_point()
+            robot_2 = self.generate_random_point()
+            if self._is_valid_point(robot_1) and self._is_valid_point(robot_2):
+                valid = True
+                sample = Point_d(4, [robot_1.x(), robot_1.y(), robot_2.x(), robot_2.y()])
+        return sample
+
+    def _steer(self, source, target, eta):
+        s1 = Point_2(source[0], source[1])
+        s2 = Point_2(source[1], source[2])
+
+        t1 = Point_2(target[0], target[1])
+        t2 = Point_2(target[1], target[2])
+
+        v1 = Vector_2(s1, t1)
+        v2 = Vector_2(s2, t2)
+
+        v_len = FT(math.sqrt(v1.squared_length().to_double() + v2.squared_length().to_double()))
+
+        if v_len <= eta:
+            return target
+        else:
+            p1 = s1 + v1 / FT(v_len * eta)
+            p2 = s2 + v2 / FT(v_len * eta)
+            return Point_d(4, [p1[0], p1[1], p2[0], p2[1]])
 
 
     def generate_random_point(self):
@@ -111,9 +199,14 @@ class Planner(object):
         return Point_2(rand_x_coord, rand_y_coord)
 
 
+
+def move_robot(prev_center, new_center, robot):
+    v = Vector_2(prev_center, new_center)
+
+
 def run_algorithm(robots, obstacles, destinations):
-    planner = Planner(robots, obstacles)
-    path = planner.generate_path(destinations)
+    planner = Planner(robots, obstacles, destinations)
+    path = planner.generate_path()
     return path
 
 def generate_path(path, robots, obstacles, destination):
@@ -134,6 +227,17 @@ TODO:
     UNDER ASSUMPTION OF SIMULTANEOUS MOVEMENT AND FIXED SPEED
     check if paths collide with each other
 3. NEAR = get nearest neighbors in that radius!
-4. COST = cost to get to point, distance in 4d?
-    
+4. Cost(v) = Cost(Parent(v)) + c(Line(Parent(v), v)). Cost(root) = 0
+
+
+
+Collision Detection:
+For every polygon, compute AABB
+Each face will have a different "name"
+Put into arrangement A
+Overlay - Free space will have no name
+        Intersection witn n AABBs will have n "names"
+
+Use point location on the arrangement A to check where the point is with which Boxes to compute intersection!
+
 """
